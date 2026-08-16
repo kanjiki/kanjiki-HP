385e0749adc5c2efa941c493f4302224b6345faf
kanjiki <kota.ayaki@gmail.com>
2026-08-17 00:59:31 +0900

    Enable GHOUL response submissions

--- ghoul-kp-style/assets/script-0.js
+++ ghoul-kp-style/assets/script-0.js
@@ -1,4 +1,2 @@
 window.GHOUL_APP_URL = location.origin + location.pathname;
-// GAS の doPost() を再デプロイするまで回答送信は停止します。
-window.GHOUL_API_URL = "";
-window.GHOUL_PENDING_API_URL = "https://script.google.com/macros/s/AKfycbzdIcT7AkIuF1qyQTAEcIqgrFuiwJZdcsiZY7TPo9KKNziWKR1R1DYkUyTAkRdqZQXaeA/exec";
+window.GHOUL_API_URL = "https://script.google.com/macros/s/AKfycbzdIcT7AkIuF1qyQTAEcIqgrFuiwJZdcsiZY7TPo9KKNziWKR1R1DYkUyTAkRdqZQXaeA/exec";
