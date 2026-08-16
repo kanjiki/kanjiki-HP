4813906038a2bdb23373ca1c6f670bade1e3a017
kanjiki <kota.ayaki@gmail.com>
2026-08-17 00:53:10 +0900

    Pause GHOUL submissions until GAS backend is redeployed

--- ghoul-kp-style/assets/script-0.js
+++ ghoul-kp-style/assets/script-0.js
@@ -1,2 +1,4 @@
 window.GHOUL_APP_URL = location.origin + location.pathname;
-window.GHOUL_API_URL = "https://script.google.com/macros/s/AKfycbzdIcT7AkIuF1qyQTAEcIqgrFuiwJZdcsiZY7TPo9KKNziWKR1R1DYkUyTAkRdqZQXaeA/exec";
+// GAS の doPost() を再デプロイするまで回答送信は停止します。
+window.GHOUL_API_URL = "";
+window.GHOUL_PENDING_API_URL = "https://script.google.com/macros/s/AKfycbzdIcT7AkIuF1qyQTAEcIqgrFuiwJZdcsiZY7TPo9KKNziWKR1R1DYkUyTAkRdqZQXaeA/exec";
