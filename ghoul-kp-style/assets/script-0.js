932ec700badc315d6974b930744034ab50afaf04
kanjiki <kota.ayaki@gmail.com>
2026-08-17 00:48:08 +0900

    Configure GHOUL frontend endpoint

---
+++ ghoul-kp-style/assets/script-0.js
@@ -0,0 +1,2 @@
+window.GHOUL_APP_URL = location.origin + location.pathname;
+window.GHOUL_API_URL = "https://script.google.com/macros/s/AKfycbzdIcT7AkIuF1qyQTAEcIqgrFuiwJZdcsiZY7TPo9KKNziWKR1R1DYkUyTAkRdqZQXaeA/exec";
